10e84671329a4c4423835934080aa9b1d11b7020
Luca Bilke <bilke@tralios.de>
2023-06-14 18:10:28 +0200

    handle directory hashes in titlestring

---
+++ lua/config/dirs.lua
@@ -0,0 +1,16 @@
+return {
+    h = '/home/luca',
+    ch = '/home/luca/.cache',
+    cf = '/home/luca/.config',
+    dt = '/home/luca/.local/share',
+    sr = '/home/luca/.local/src',
+    it = '/home/luca/.local/src/git',
+    sc = '/home/luca/.local/bin',
+    mn = '/mnt',
+    dl = '/home/luca/Downloads',
+    dm = '/home/luca/Documents',
+    ms = '/home/luca/Music',
+    pc = '/home/luca/Pictures',
+    vd = '/home/luca/Videos',
+    dot = '/home/luca/.local/share/stow/dots',
+}
